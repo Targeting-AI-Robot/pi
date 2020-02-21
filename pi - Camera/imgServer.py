ab6983a5ca67e176cd0be82ef88ba59d2f44d574
Ho <wnsgh578@naver.com>
2020-02-21 18:16:13 +0900

    camera server

--- pi - Camera/imgServer.py
+++ pi - Camera/imgServer.py
@@ -1,6 +1,6 @@
 import socket
 import cv2
-import numpy
+import numpy as np
 
 TCP_IP = ''
 TCP_PORT = 8282
@@ -11,17 +11,30 @@ sock.bind((TCP_IP, TCP_PORT))
 sock.listen(True)
 conn, addr = sock.accept()
 
-# just read jpg frame at test
-frame = cv2.imread('testImg.jpg')
 encode_param=[int(cv2.IMWRITE_JPEG_QUALITY),90]
-result, imgencode = cv2.imencode('.jpg', frame, encode_param)
-data = numpy.array(imgencode)
-stringData = data.tostring()
 
+# get two frame and return
+# return frame1, frame2
+# Not implemented yet
+def getFrame():
+	return None, None
 
 while True:
     req = conn.recv(1024)
 	# send frame when it receive 'req'
     if req.decode() == 'req':
-        conn.send( str(len(stringData)).ljust(16));
-        conn.send( stringData );
+		frame1, frame2 = getFrame()
+		
+		result1, imgencode1 = cv2.imencode('.jpg', frame1, encode_param)
+		data1 = np.array(imgencode1)
+		stringData1 = data1.tostring()
+		
+		result2, imgencode2 = cv2.imencode('.jpg', frame2, encode_param)
+		data2 = np.array(imgencode2)
+		stringData2 = data2.tostring()
+	
+        conn.send( str(len(stringData1)).ljust(16));
+        conn.send( stringData1 );
+		
+		conn.send( str(len(stringData2)).ljust(16));
+        conn.send( stringData2 );
